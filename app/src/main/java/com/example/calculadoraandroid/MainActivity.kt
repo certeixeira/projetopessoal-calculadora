015fee14a2e117c7f581e7f0e1f7987bb82de561
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2021-08-12 21:42:38 -0300

    Finalizado parte lógica da calculadora e correção de bug com o "."

--- app/src/main/java/com/example/calculadoraandroid/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadoraandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calculadoraandroid.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,93 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bntAc.setOnClickListener {
+            binding.inputtext.text = ""
+            binding.outputtext.text = ""
+        }
+
+        binding.btn0.setOnClickListener {
+            binding.inputtext.append("0")
+        }
+
+        binding.btn1.setOnClickListener {
+            binding.inputtext.append("1")
+        }
+
+        binding.btn2.setOnClickListener {
+            binding.inputtext.append("2")
+        }
+
+        binding.btn3.setOnClickListener {
+            binding.inputtext.append("3")
+        }
+
+        binding.btn4.setOnClickListener {
+            binding.inputtext.append("4")
+        }
+
+        binding.btn5.setOnClickListener {
+            binding.inputtext.append("5")
+        }
+
+        binding.btn6.setOnClickListener {
+            binding.inputtext.append("6")
+        }
+
+        binding.btn7.setOnClickListener {
+            binding.inputtext.append("7")
+        }
+
+        binding.btn8.setOnClickListener {
+            binding.inputtext.append("8")
+        }
+
+        binding.btn9.setOnClickListener {
+            binding.inputtext.append("9")
+        }
+
+        binding.btnDot.setOnClickListener {
+            binding.inputtext.append(".")
+        }
+
+        binding.btnPlus.setOnClickListener {
+            binding.inputtext.append("+")
+        }
+
+        binding.btnDivide.setOnClickListener {
+            binding.inputtext.append("/")
+        }
+
+        binding.btnMinus.setOnClickListener {
+            binding.inputtext.append("-")
+        }
+
+        binding.btnMultiplication.setOnClickListener {
+            binding.inputtext.append("*")
+        }
+
+        binding.btnStartBracket.setOnClickListener {
+            binding.inputtext.append("(")
+        }
+
+        binding.bttnEndBracket.setOnClickListener {
+            binding.inputtext.append(")")
+        }
+
+        binding.btnEqual.setOnClickListener {
+
+            val expression = ExpressionBuilder(binding.inputtext.text.toString()).build()
+            val result = expression.evaluate()
+            val longresult = result.toLong()
+
+            if (result == longresult.toDouble()) {
+                binding.outputtext.text =longresult.toString()
+
+            } else {
+                binding.outputtext.text = result.toString()
+            }
+        }
+
     }
 }
